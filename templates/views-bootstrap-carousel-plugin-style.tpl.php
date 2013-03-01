d888b86aa7f61dc6d7cad89db58fc4218b28f50d
mrded <dmitry@demenchuk.me>
2013-03-01 10:12:36 +0000

    Delete link field & disable settings for style plugin.

--- templates/views-bootstrap-carousel-plugin-style.tpl.php
+++ templates/views-bootstrap-carousel-plugin-style.tpl.php
@@ -1,7 +1,7 @@
-<div id="myCarousel" class="carousel slide <?php print $classes ?>">
+<div id="views-bootstrap-carousel<?php print $id ?>" class="carousel slide <?php print $classes ?>">
   <ol class="carousel-indicators">
     <?php foreach ($rows as $key => $value): ?>
-      <li data-target="#myCarousel" data-slide-to="<?php print $key ?>" class="<?php if ($key === 0) print 'active' ?>"></li>
+      <li data-target="#views-bootstrap-carousel<?php print $id ?>" data-slide-to="<?php print $key ?>" class="<?php if ($key === 0) print 'active' ?>"></li>
     <?php endforeach ?>
   </ol>
   <!-- Carousel items -->
@@ -13,6 +13,6 @@
     <?php endforeach ?>
   </div>
   <!-- Carousel nav -->
-  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+  <a class="carousel-control left" href="#views-bootstrap-carousel<?php print $id ?>" data-slide="prev">&lsaquo;</a>
+  <a class="carousel-control right" href="#views-bootstrap-carousel<?php print $id ?>" data-slide="next">&rsaquo;</a>
 </div>
