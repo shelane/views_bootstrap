d527bce6fff9892058169456590fe24223778c84
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-07-02 00:46:55 +0100

    Add options for carousel.

--- templates/carousel/views-bootstrap-carousel-plugin-style.tpl.php
+++ templates/carousel/views-bootstrap-carousel-plugin-style.tpl.php
@@ -1,9 +1,13 @@
-<div id="views-bootstrap-carousel-<?php print $id ?>" class="carousel slide <?php print $classes ?>">
-  <ol class="carousel-indicators">
-    <?php foreach ($rows as $key => $value): ?>
-      <li data-target="#views-bootstrap-carousel-<?php print $id ?>" data-slide-to="<?php print $key ?>" class="<?php if ($key === 0) print 'active' ?>"></li>
-    <?php endforeach ?>
-  </ol>
+<div id="views-bootstrap-carousel-<?php print $id ?>" class="<?php print $classes ?>" <?php print $attributes ?>>
+  <?php if ($indicators): ?>
+    <!-- Carousel indicators -->
+    <ol class="carousel-indicators">
+      <?php foreach ($rows as $key => $value): ?>
+        <li data-target="#views-bootstrap-carousel-<?php print $id ?>" data-slide-to="<?php print $key ?>" class="<?php if ($key === 0) print 'active' ?>"></li>
+      <?php endforeach ?>
+    </ol>
+  <?php endif ?>
+
   <!-- Carousel items -->
   <div class="carousel-inner">
     <?php foreach ($rows as $key => $row): ?>
@@ -12,7 +16,10 @@
       </div>
     <?php endforeach ?>
   </div>
-  <!-- Carousel nav -->
-  <a class="carousel-control left" href="#views-bootstrap-carousel-<?php print $id ?>" data-slide="prev">&lsaquo;</a>
-  <a class="carousel-control right" href="#views-bootstrap-carousel-<?php print $id ?>" data-slide="next">&rsaquo;</a>
+
+  <?php if ($navigation): ?>
+    <!-- Carousel navigation -->
+    <a class="carousel-control left" href="#views-bootstrap-carousel-<?php print $id ?>" data-slide="prev">&lsaquo;</a>
+    <a class="carousel-control right" href="#views-bootstrap-carousel-<?php print $id ?>" data-slide="next">&rsaquo;</a>
+  <?php endif ?>
 </div>
