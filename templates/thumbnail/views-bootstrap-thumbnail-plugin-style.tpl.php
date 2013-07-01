48591d81321fddac0c26d008830e297152695a4c
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-07-01 09:38:44 +0100

    Change $column_type variable. Now this variable will pass column type number instead of a class name.

--- templates/thumbnail/views-bootstrap-thumbnail-plugin-style.tpl.php
+++ templates/thumbnail/views-bootstrap-thumbnail-plugin-style.tpl.php
@@ -1,6 +1,19 @@
+<?php
+/**
+ * @file views-bootstrap-thumbnail-plugin-style.tpl.php
+ * Default simple view template to display Bootstrap Thumbnails.
+ *
+ * - $rows contains a nested array of rows. Each row contains an array of
+ *   columns.
+ * - $column_type contains a number (default Bootstrap grid system column type).
+ *
+ * @ingroup views_templates
+ */
+?>
+
 <ul id="views-bootstrap-thumbnail-<?php print $id ?>" class="<?php print $classes ?>">
   <?php foreach ($rows as $key => $row): ?>
-    <li class="<?php print $column_type ?>">
+    <li class="span<?php print $column_type ?>">
       <div class="thumbnail">
         <?php print $row ?>
       </div>
