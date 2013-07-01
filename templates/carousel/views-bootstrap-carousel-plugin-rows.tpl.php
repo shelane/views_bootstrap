5aa2335c7c0b438185b7dc103cfeccea40906098
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-07-01 12:04:23 +0100

    Check $title & $description before render caption.

--- templates/carousel/views-bootstrap-carousel-plugin-rows.tpl.php
+++ templates/carousel/views-bootstrap-carousel-plugin-rows.tpl.php
@@ -1,11 +1,13 @@
 <?php print $image ?>
 
-<div class="carousel-caption">
-  <?php if ($title): ?>
-    <h4><?php print $title ?></h4>
-  <?php endif ?>
+<?php if ($title || $description): ?>
+  <div class="carousel-caption">
+    <?php if ($title): ?>
+      <h4><?php print $title ?></h4>
+    <?php endif ?>
 
-  <?php if ($description): ?>
-    <p><?php print $description ?></p>
-  <?php endif ?>
-</div>
+    <?php if ($description): ?>
+      <p><?php print $description ?></p>
+    <?php endif ?>
+  </div>
+<?php endif ?>
