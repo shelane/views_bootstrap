f4a34012e4ae8497bdfcecfad3531057419310a3
othermachines <othermachines@1201100.no-reply.drupal.org>
2013-08-26 14:20:41 +0100

    PHP Notices then title and description is empty

--- templates/carousel/views-bootstrap-carousel-plugin-rows.tpl.php
+++ templates/carousel/views-bootstrap-carousel-plugin-rows.tpl.php
@@ -1,12 +1,12 @@
 <?php print $image ?>
 
-<?php if ($title || $description): ?>
+<?php if (!empty($title) || !empty($description)): ?>
   <div class="carousel-caption">
-    <?php if ($title): ?>
+    <?php if (!empty($title)): ?>
       <h4><?php print $title ?></h4>
     <?php endif ?>
 
-    <?php if ($description): ?>
+    <?php if (!empty($description)): ?>
       <p><?php print $description ?></p>
     <?php endif ?>
   </div>
