c55affd82b691d1dc40593ec7652d4da3dd34820
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-09-29 13:25:32 +0100

    Issue #2199375 by jmoratti, Andre-B, subhojit777, robertom: Add support for different grid column sizes

--- templates/grid/views-bootstrap-grid-plugin-style.tpl.php
+++ templates/grid/views-bootstrap-grid-plugin-style.tpl.php
@@ -19,7 +19,7 @@
     <?php foreach ($items as $row): ?>
       <div class="row">
         <?php foreach ($row['content'] as $column): ?>
-          <div class="col col-lg-<?php print $column_type ?>">
+          <div class="col <?php print $class_prefix; ?>-<?php print $column_type ?>">
             <?php print $column['content'] ?>
           </div>
         <?php endforeach ?>
@@ -30,7 +30,7 @@
 
     <div class="row">
       <?php foreach ($items as $column): ?>
-        <div class="col col-lg-<?php print $column_type ?>">
+        <div class="col <?php print $class_prefix; ?>-<?php print $column_type ?>">
           <?php foreach ($column['content'] as $row): ?>
             <?php print $row['content'] ?>
           <?php endforeach ?>
