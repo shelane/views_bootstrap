dc127be72fda955eb1420276ac583324663a12ed
Dmitry Demenchuk <dmitry@demenchuk.me>
2015-09-24 14:20:24 +0200

    Issue #2363967: Undefined variable: titles in include() - Accordion plugin

--- templates/accordion/views-bootstrap-accordion-plugin-style.tpl.php
+++ templates/accordion/views-bootstrap-accordion-plugin-style.tpl.php
@@ -4,21 +4,23 @@
 
 <div id="views-bootstrap-accordion-<?php print $id ?>" class="<?php print $classes ?>">
   <?php foreach ($rows as $key => $row): ?>
-    <div class="accordion-group">
-      <div class="accordion-heading">
-        <a class="accordion-toggle"
-           data-toggle="collapse"
-           data-parent="#views-bootstrap-accordion-<?php print $id ?>"
-           href="#collapse-<?php print $id . '-' . $key ?>">
-          <?php print $titles[$key] ?>
-        </a>
-      </div>
-
-      <div id="collapse-<?php print $id . '-' . $key ?>" class="accordion-body collapse">
-        <div class="accordion-inner">
-          <?php print $row ?>
+    <?php if (isset($titles[$key])): ?>
+      <div class="accordion-group">
+        <div class="accordion-heading">
+          <a class="accordion-toggle"
+            data-toggle="collapse"
+            data-parent="#views-bootstrap-accordion-<?php print $id ?>"
+            href="#collapse-<?php print $id . '-' . $key ?>">
+            <?php print $titles[$key] ?>
+          </a>
+        </div>
+  
+        <div id="collapse-<?php print $id . '-' . $key ?>" class="accordion-body collapse">
+          <div class="accordion-inner">
+            <?php print $row ?>
+          </div>
         </div>
       </div>
-    </div>
+    <?php endif ?>
   <?php endforeach ?>
 </div>
