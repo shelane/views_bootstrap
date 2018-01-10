fdb53b4a56cf031ab6257d637d310f62ff5d1185
ShaunDychko <ShaunDychko@475828.no-reply.drupal.org>
2018-01-10 16:06:51 +0000

    Issue #2277797 by ShaunDychko, seworthi, Adam_M: FR: Support for Bootstrap Panels

--- js/views-bootstrap-carousel.js
+++ js/views-bootstrap-carousel.js
@@ -16,7 +16,44 @@
         catch(err) {
           console.log(err);
         }
+
+        var myCarousel = $('#views-bootstrap-carousel-' + carousel.id, context);
+
+        // If the carousel is not set to wrap hide the left control on the
+        // first slide and the right control on the last slide.
+        if (!myCarousel.carousel.attributes.wrap) {
+          // Update the carousel controls when the page loads.
+          updateControls(myCarousel);
+
+          // Update the carousel controls each time the slide changes.
+          myCarousel.on("slid.bs.carousel", "", function(event) {
+            updateControls(myCarousel)
+          });
+        }
       });
     }
   };
+
+  /**
+   * Update carousel controls.
+   *
+   * Hide the left control on the first slide.
+   * Hide the right control on the last slide.
+   * For all other slides, show both controls.
+   *
+   * @param carousel
+   *   The carousel to update.
+   */
+  function updateControls(carousel) {
+    if ($(".carousel-inner .item:first", carousel).hasClass("active")) {
+      carousel.children(".carousel-control.left").hide();
+      carousel.children(".carousel-control.right").show();
+    } else if ($(".carousel-inner .item:last", carousel).hasClass("active")) {
+      carousel.children(".carousel-control.right").hide();
+      carousel.children(".carousel-control.left").show();
+    } else {
+      carousel.children(".carousel-control").show();
+    }
+  }
+
 })(jQuery);
